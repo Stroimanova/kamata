64d82397e34d2b3cf0514f6e811f1a13297bacdc
unknown <simonastroimanova@hotmail.com>
2023-12-04 18:31:03 +0100

    first java program

---
+++ Kamata.java
@@ -0,0 +1,19 @@
+package project_17;
+
+import java.util.Scanner;
+
+public class Kamata {
+
+	public static void main(String[] args) {
+		double depozit; // poceten depozit
+		double kamatnastapka; // godisna kamatna stapka
+		double iznoskamata; // paricen iznos na kamata
+		Scanner tastatura = new Scanner(System.in);
+		System.out.println("Vnesi poceten depozit: ");
+		depozit = tastatura.nextDouble();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
